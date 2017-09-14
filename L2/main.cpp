3745007619ae9008b7d5b6ebe41ea8f41f0f24c2
Marc <Marc@128.175.252.215>
2017-09-14 17:33:36 -0400

    initial commit

---
+++ L2/main.cpp
@@ -0,0 +1,24 @@
+/*
+ * Marc Bolinas
+ * Brian Philips
+ *
+ * NAME OF TA
+ *
+ * 9/14/17
+ * LAB 2
+ *
+ *
+ *
+ */
+
+
+
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+
+
+int main(){
+	return 0;
+}
