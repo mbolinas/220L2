ff245bf0d3bbd8842117703215e052194239ddad
Marc <Marc@128.175.252.215>
2017-09-20 22:00:07 -0400

    final commit

--- L2/main.cpp
+++ L2/main.cpp
@@ -19,54 +19,49 @@
 using namespace std;
 
 
+//PART 1
 
-string reverseString(string str);
+string reverse_string(string str); //returns the reverse of the string passed
 
-int sumDigit(int i);
+int sum_digit(int i); //returns sum of all the individual 'digits' from int passed
 
-int find_min(int array[], int array_size);
+int find_min(int array[], int array_size); //gets minimum value out of passed array
+int find_min_helper(int array[], int array_size, int current); //recursively finds the minimum value in an array for find_min()
 
-int find_min_helper(int array[], int array_size, int current);
-
-bool is_elfish(string str);
-
-bool is_elfish_helper(string str, string key);
+bool is_elfish(string str); //true if passed string contains "e" "l" and "f" in any order
+bool is_in_string(string str, string key); //recursively finds 'key' in 'str', returns true if found. helper function for is_elfish()
 
 bool is_perfect_number(int i);
-
 int is_perfect_number_helper(int i, int current);
+bool is_divisor(int top, int bottom); //true if numerator is perfectly divisible by denominator
 
-bool is_divisor(int top, int bottom);
-
-void gon_give_x_to_ya(int i);
-
-void x_helper(int start, int end, int offset, int spacing);
-
-void print_space(int spacing);
+void print_x(int i); //prints out an X made of X's whose size depends on passed int
+void x_helper(int start, int end, int offset, int spacing); //recursive helper function for print_x()
+void print_space(int spacing); //recursively prints spaces for x_helper()
 
 
 
+//PART 2
 
+bool order1(int *i, int *j); //true if i < j, otherwise false and switches i and j. done via pass by pointer
 
-bool order1(int *i, int *j);
+bool order2(int &k, int &l); //true if k < l, otherwise false and switches k and l. done via pass by reference
 
-bool order2(int &k, int &l);
+void order3(int *i); //prints out value, memory address of parameter, and memory address of variable via pass by pointer
 
-void order3(int *i);
+void order4(int &i); //prints out value, memory address of parameter, and memory address of variable via pass by reference
 
-void order4(int &i);
+void array_filler(int *array, int array_size); //fills given array with randomly generated numbers from 0 to 100
 
-void array_filler(int *array, int array_size);
+void array_printer(int *array, int array_size); //prints out all values in the array
 
-void array_printer(int *array, int array_size);
+void problem7(int *array, int array_size, int &k, int &j); //finds smallest value in array, sets k to that value and j to the index in its array
 
-void problem7(int *array, int array_size, int &k, int &j);
+void array_address_printer(int *array, int array_size); //prints out all addresses in an int array
+//gotta love function overloading
+void array_address_printer(double *array, int array_size); //prints out all addresses in a double array
 
-void array_address_printer(int *array, int array_size);
-
-void array_address_printer(double *array, int array_size);
-
-int p10(int *a1, int *a2, int size1, int size2, int &sum1, int &sum2);
+int p10(int *a1, int *a2, int size1, int size2, int &sum1, int &sum2); //calculates sum of arrays 1 & 2, sets their sums to sum1 and sum2. returns 1 if sum1 > sum2, 0 if =, and -1 if sum1 < sum2
 
 
 
@@ -77,54 +72,204 @@ int p10(int *a1, int *a2, int size1, int size2, int &sum1, int &sum2);
 
 
 int main(){
-
-//	int arr[5] = {1,2,0,4,5};
-//
-//	cout << find_min(arr, 5);
-//
-//	string s = "hello";
-//
-//	cout << s.substr(0,1);
-//
-//	cout << (s.substr(0,1) == "h");
-
-//	cout << is_perfect_number(8128);
-
-	gon_give_x_to_ya(5);
+	srand(time(NULL));
 
 
-	int x = 3;
-	int y = 2;
-	cout << &x << endl;
-	order4(x);
+
+	cout << "PROBLEM 1" << endl;
+	cout << reverse_string("olleh") << endl;
+	cout << reverse_string("marc bolinas") << endl;
+	cout << reverse_string("brian philips") << endl;
+	cout << "********************************************************" << endl
+			<< endl;
+
+
+
+	cout << "PROBLEM 2" << endl;
+	cout << sum_digit(123) << endl;
+	cout << sum_digit(0) << endl;
+	cout << sum_digit(420) << endl;
+	cout << "********************************************************" << endl
+			<< endl;
+
+
+	cout << "PROBLEM 3" << endl;
+	int a1[5] = {1,2,3,4,5};
+	int a2[5] = {7,2,5,2,6};
+	int a3[5] = {3,9,4,7,3};
+	cout << find_min(a1,5) << endl;
+	cout << find_min(a2,5) << endl;
+	cout << find_min(a3,5) << endl;
+	cout << "********************************************************" << endl
+			<< endl;
+
+
+
+	cout << "PROBLEM 4" << endl;
+	cout << is_elfish("hello") << endl;
+	cout << is_elfish("elfy boy") << endl;
+	cout << is_elfish("a;ldfgh;rheglk") << endl;
+	cout << "********************************************************" << endl
+			<< endl;
+
+
+
+	cout << "PROBLEM 5" << endl;
+	cout << is_perfect_number(496) << endl;
+	cout << is_perfect_number(497) << endl;
+	cout << is_perfect_number(23425) << endl;
+	cout << "********************************************************" << endl
+			<< endl;
+
+
+
+	cout << "PROBLEM 6" << endl;
+	print_x(3);
+	cout << endl;
+	print_x(5);
+	cout << endl;
+	print_x(2);
+	cout << endl;
+	cout << "********************************************************" << endl
+			<< endl;
+
+
+
+	cout << "PROBLEM 1" << endl;
+	int x = rand() % 51;
+	int y = rand() % 51;
+	cout << "X is " << x << endl;
+	cout << "Y is " << y << endl;
+	cout << order1(&x, &y) << endl;
+	cout << "X is now " << x << endl;
+	cout << "Y is now " << y << endl;
+	cout << "********************************************************" << endl
+			<< endl;
+
+
+
+	cout << "PROBLEM 2" << endl;
+	int x2 = rand() % 51;
+	int y2 = rand() % 51;
+	cout << "X2 is " << x << endl;
+	cout << "Y2 is " << y << endl;
+	cout << order2(x2, y2) << endl;
+	cout << "X2 is now " << x << endl;
+	cout << "Y2 is now " << y << endl;
+	cout << "********************************************************" << endl
+			<< endl;
+
+
+
+	cout << "PROBLEM 3" << endl;
+	int z = 34;
+	cout << &z << endl;
+	order3(&z);
+	cout << "********************************************************" << endl
+			<< endl;
+
+
+
+	cout << "PROBLEM 4" << endl;
+	int a = 23;
+	cout << "Value is " << a << " and address is " << &a << endl;
+	order4(a);
+	cout << "********************************************************" << endl
+			<< endl;
+
+
+
+	cout << "PROBLEM 5" << endl;
+	int arr20[20];
+	array_filler(arr20,20);
+	cout << arr20[10] << endl;
+	cout << "********************************************************" << endl
+			<< endl;
+
+
+
+	cout << "PROBLEM 6" << endl;
+	array_printer(arr20,20);
+	array_printer(a1,5);
+	array_printer(a2,5);
+	cout << "********************************************************" << endl
+			<< endl;
+
+
+
+	cout << "PROBLEM 7" << endl;
+	int g = -1;
+	int h = -1;
+	problem7(a1,5,g,h);
+	cout << "Smallest value is " << g << " and it's index is " << h << endl;
+	cout << "********************************************************" << endl
+			<< endl;
+
+
+
+	cout << "PROBLEM 8" << endl;
+	array_address_printer(a1,5);
+	array_address_printer(a2,5);
+	array_address_printer(a3,5);
+	cout << "********************************************************" << endl
+			<< endl;
+
+
+
+	cout << "PROBLEM 9" << endl;
+	double da1[5] = {1.1,2.2,3.3,4.4,5.5};
+	double da2[5] = {5.5,3.3,2.2,6.6,4.4};
+	double da3[5] = {8.8,2.2,5.5,3.3,7.7};
+	array_address_printer(da1,5);
+	array_address_printer(da2,5);
+	array_address_printer(da3,5);
+	cout << "********************************************************" << endl
+			<< endl;
+
+
+
+	cout << "PROBLEM 10" << endl;
+	cout << p10(a1,a2,5,5,g,h) << endl;
+	cout << "Sum1 is " << g << " and sum2 is " << h << endl;
+	cout << p10(a2,a3,5,5,g,h) << endl;
+	cout << "Sum1 is " << g << " and sum2 is " << h << endl;
+	cout << p10(a3,a1,5,5,g,h) << endl;
+	cout << "Sum1 is " << g << " and sum2 is " << h << endl;
+	cout << "********************************************************" << endl
+			<< endl;
+
 
 	return 0;
 }
 
-string reverseString(string str){
+
+//Problem 1
+string reverse_string(string str){
 	if(str.length() <= 0){
 		return "";
 	}
 	else{
-		string temp = str.substr(str.length() - 1, 1);
-		str.pop_back();
-		return temp + reverseString(str);
+		string temp = str.substr(str.length() - 1, 1);	//last character in str
+		str.pop_back(); //remove last character from string
+		return temp + reverse_string(str);
 	}
 }
 
 
-int sumDigit(int i){
+//Problem 2
+int sum_digit(int i){
 	if(i == 0){
 		return 0;
 	}
 	else{
-		return (i % 10) + sumDigit(i / 10);
+		return (i % 10) + sum_digit(i / 10); //i % 10 gets rightmost digit, i / 10 shifts all digits to the right. repeat until 0
 	}
 }
 
 
+//Problem 3
 int find_min(int array[], int array_size){
-	return find_min_helper(array, array_size, array[0]);
+	return find_min_helper(array, array_size, array[0]); //calls helper function where the recursion is
 }
 
 int find_min_helper(int *array, int array_size, int current){
@@ -137,14 +282,15 @@ int find_min_helper(int *array, int array_size, int current){
 }
 
 
-bool is_elfish(string str){
-	return (is_elfish_helper(str, "e") &&
-			is_elfish_helper(str, "l") &&
-			is_elfish_helper(str, "f"));
+//Problem 4
+bool is_elfish(string str){ //calls helper function where the recursion is
+	return (is_in_string(str, "e") &&
+			is_in_string(str, "l") &&
+			is_in_string(str, "f"));
 }
 
-bool is_elfish_helper(string str, string key){
-	if(str.substr(str.length() - 1,1) == key){
+bool is_in_string(string str, string key){ //generalized recursive version of is_elfish() - true if 'key' is in 'str'
+	if(str.substr(str.length() - 1, key.length()) == key){
 		return true;
 	}
 	else if(str.length() <= 1){
@@ -152,36 +298,38 @@ bool is_elfish_helper(string str, string key){
 	}
 	else{
 		str.pop_back();
-		return is_elfish_helper(str, key);
+		return is_in_string(str, key);
 	}
 }
 
 
-bool is_perfect_number(int i){
+//Problem 5
+bool is_perfect_number(int i){ //calls helper function where recursion is
 	return (i == is_perfect_number_helper(i, 1));
 }
 
 
-int is_perfect_number_helper(int i, int current){
+int is_perfect_number_helper(int i, int current){ //goes through all numbers from 1 to i
 	if(current >= i){
 		return 0;
 	}
 	else{
-		if(is_divisor(i, current)){
+		if(is_divisor(i, current)){ //if current is a divisor, then add it to sum and keep recursing
 			return current + is_perfect_number_helper(i, current + 1);
 		}
-		else{
+		else{ //otherwise just recurse and don't add to sum
 			return is_perfect_number_helper(i, current + 1);
 		}
 	}
 }
 
-bool is_divisor(int top, int bottom){
+bool is_divisor(int top, int bottom){ //true if numerator is perfectly divisble by denominator
 	return !(top % bottom);
 }
 
 
-void gon_give_x_to_ya(int i){
+//Problem 6
+void print_x(int i){ //calls helper function
 	int start = i - 1;
 	int end = start - (2 * start);
 	int spacing = start * 2 - 1;
@@ -190,34 +338,39 @@ void gon_give_x_to_ya(int i){
 
 void x_helper(int start, int end, int offset, int spacing){
 	if(start >= end){
-		print_space(offset);
+
+		print_space(offset); //amount of spaces before the first x
 		cout << "x";
-		print_space(spacing);
-		if(spacing != 0){
+		print_space(spacing); //amount of spaces between the two x's
+		if(spacing != 0){ //in the middle there is only one x. is space = 0 we only need one x
 			cout << "x";
 		}
 		cout << endl;
+
+
 		if(start > 0){
-			offset++;
+			offset++; //as we go towards the middle, offset increases
 		}
 		else if(start <= 0){
-			offset--;
+			offset--; //if we're past the middle x, offset decreases instead
 		}
 		start--;
-		if(start == 0){
+
+
+		if(start == 0){ //if we're in the middle x, spacing is 0
 			spacing = 0;
 		}
-		else if(start == -1){
+		else if(start == -1){ //spacing specifically decrements by 1 for this
 			spacing = -1;
 		}
-		else{
+		else{ //normally spacing decrements by 2 each time
 			spacing -= 2;
 		}
 		x_helper(start, end, offset, spacing);
 	}
 }
 
-void print_space(int spacing){
+void print_space(int spacing){ //prints a space, recursively gets count to 0 (whether from negative of positive)
 	if(spacing > 0){
 		cout << " ";
 		print_space(spacing - 1);
@@ -234,9 +387,8 @@ void print_space(int spacing){
 
 
 
-
-
-bool order1(int *i, int *j){
+//Problem 1
+bool order1(int *i, int *j){ //true if i < j, else false and switch values via call by pointer
 	if(*i < *j){
 		return true;
 	}
@@ -249,7 +401,8 @@ bool order1(int *i, int *j){
 }
 
 
-bool order2(int &k, int &l){
+//Problem 2
+bool order2(int &k, int &l){ //true if k < l, else false and switch values via call by reference
 	if(k < l){
 		return true;
 	}
@@ -262,31 +415,34 @@ bool order2(int &k, int &l){
 }
 
 
-void order3(int *i){
+//Problem 3
+void order3(int *i){ //prints out value, address of parameter, and address of variable via call by pointer
 	cout << *i << endl;
 	cout << i << endl;
 	cout << &i << endl;
 }
 
 
-void order4(int &i){
+//Problem 4
+void order4(int &i){ //prints out value, and address of parameter via call by reference
 	cout << i << endl;
 	cout << &i << endl;
 }
 
 
-void array_filler(int *array, int array_size){
-	srand(time(NULL));
+//Problem 5
+void array_filler(int *array, int array_size){ //fills array with random ints from 0 to 100
 	for(int i = 0; i < array_size; i++){
 		array[i] = (rand() % 100);
 	}
 }
 
 
-void array_printer(int *array, int array_size){
+//Problem 6
+void array_printer(int *array, int array_size){ //prints out all values of array
 	for(int i = 0; i < array_size; i++){
 		cout << array[i];
-		if(i < array_size - 1){
+		if(i < array_size - 1){ //don't want to print a comma if there's nothing else in the array
 			cout << ", ";
 		}
 	}
@@ -294,7 +450,8 @@ void array_printer(int *array, int array_size){
 }
 
 
-void problem7(int *array, int array_size, int &k, int &j){
+//Problem 7
+void problem7(int *array, int array_size, int &k, int &j){  //finds smallest value in array, sets k to that value and j to the index in its array
 	int smallest = array[0];
 	int index = 0;
 	int i = 0;
@@ -310,21 +467,24 @@ void problem7(int *array, int array_size, int &k, int &j){
 }
 
 
-void array_address_printer(int *array, int array_size){
+//Problem 8
+void array_address_printer(int *array, int array_size){ //prints out all addresses in an int array
 	for(int i = 0; i < array_size; i++){
 		cout << "Element #" << i << ": " << &array[i] << endl;
 	}
 }
 
 
-void array_address_printer(double *array, int array_size){
+//Problem 9
+void array_address_printer(double *array, int array_size){ //prints out all addresses in a double array
 	for(int i = 0; i < array_size; i++){
 		cout << "Element #" << i << ": " << &array[i] << endl;
 	}
 }
 
 
-int p10(int *a1, int *a2, int size1, int size2, int &sum1, int &sum2){
+//Problem 10
+int p10(int *a1, int *a2, int size1, int size2, int &sum1, int &sum2){ //calculates sum of arrays 1 & 2, sets their sums to sum1 and sum2. returns 1 if sum1 > sum2, 0 if =, and -1 if sum1 < sum2
 	for(int i = 0; i < size1; i++){
 		sum1 = sum1 + a1[i];
 	}
